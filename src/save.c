88459ef34b32510fbc38defffcea0ed5c86b6cb4
David Laban <david.laban@collabora.co.uk>
2011-06-12 01:14:07 -0400

    Add mechanism to save playlists asyncronously
    currently doesn't actually do anything.

--- src/save.c
+++ src/save.c
@@ -26,11 +26,19 @@
 #include "git-spot.h"
 #include "cmd.h"
 
+typedef void (*sg_callback) (void *user_data);
+
+static void container_loaded(sp_playlistcontainer *pc, void *user_data);
+static void save_playlist_async(sp_playlist *playlist, const char *directory,
+    sg_callback cb, void *user_data);
+
 typedef struct {
   sp_playlistcontainer *pc;
   int argc;
   char **argv;
   sp_playlistcontainer_callbacks *callbacks;
+  int started_calls;
+  int finished_calls;
 } container_context;
 
 static container_context *container_context_new(
@@ -44,6 +52,8 @@ static container_context *container_context_new(
   ret->argv = argv;
   ret->callbacks = malloc(sizeof(sp_playlistcontainer_callbacks));
   memset(ret->callbacks, 0, sizeof(sp_playlistcontainer_callbacks));
+  ret->started_calls = 0;
+  ret->finished_calls = 0;
   return ret;
 }
 
@@ -55,7 +65,23 @@ static void container_context_free(container_context *ctx) {
 
 static int subscriptions_updated;
 
-static void container_loaded(sp_playlistcontainer *pc, void *userdata);
+static void cmd_save_finally(container_context *ctx)
+{
+  cmd_logout(ctx->argc, ctx->argv);
+  container_context_free(ctx);
+}
+
+static void container_context_start_call(container_context *ctx)
+{
+  ctx->started_calls ++;
+}
+
+static void container_context_finish_call(container_context *ctx)
+{
+  ctx->finished_calls ++;
+  if(ctx->finished_calls == ctx->started_calls)
+    cmd_save_finally(ctx);
+}
 
 /**
  *
@@ -76,12 +102,12 @@ int cmd_save(int argc, char **argv)
 static void container_loaded(sp_playlistcontainer *pc, void *userdata)
 {
   container_context *ctx = userdata;
-  int argc = ctx->argc;
   char **argv = ctx->argv;
 	int i, j, level = 0;
 	sp_playlist *pl;
 	char name[200];
 
+	container_context_start_call(ctx);
   printf("path = %s\n", argv[1]);
 	printf("%d entries in the container\n", sp_playlistcontainer_num_playlists(pc));
 
@@ -91,6 +117,8 @@ static void container_loaded(sp_playlistcontainer *pc, void *userdata)
 				printf("%d. ", i);
 				for (j = level; j; --j) printf("\t");
 				pl = sp_playlistcontainer_playlist(pc, i);
+				container_context_start_call(ctx);
+				save_playlist_async(pl, "", (sg_callback)container_context_finish_call, ctx);
 				printf("%s", sp_playlist_name(pl));
 				if(subscriptions_updated)
 					printf(" (%d subscribers)", sp_playlist_num_subscribers(pl));
@@ -115,9 +143,56 @@ static void container_loaded(sp_playlistcontainer *pc, void *userdata)
 				break;
 		}
 	}
-  cmd_logout(argc, argv);
-  container_context_free(ctx);
+
+  printf("Made %d async calls.\n", ctx->started_calls -1);
+  container_context_finish_call(ctx);
 }
+
+typedef struct {
+  sp_playlist *playlist;
+  char *directory;
+  sg_callback cb;
+  void *user_data;
+} playlist_data;
+
+static playlist_data *playlist_data_new(sp_playlist *playlist,
+    const char *directory,
+    sg_callback cb,
+    void *user_data)
+{
+  playlist_data *data = malloc(sizeof(playlist_data));
+
+  data->playlist = playlist;
+  data->directory = strdup(directory);
+  data->cb = cb;
+  data->user_data = user_data;
+
+  return data;
+}
+
+static void playlist_data_free(playlist_data *data)
+{
+  free(data->directory);
+  free(data);
+}
+
+static void save_playlist_finally(playlist_data *data)
+{
+  if(data->cb != NULL)
+    data->cb(data->user_data);
+  playlist_data_free(data);
+}
+
+static void save_playlist_async(sp_playlist *playlist,
+    const char *directory,
+    sg_callback cb,
+    void *user_data)
+{
+  playlist_data *data = playlist_data_new(playlist, directory, cb, user_data);
+
+  save_playlist_finally(data);
+}
+
 
 /**
  *
