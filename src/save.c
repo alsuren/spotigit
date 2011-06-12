646c1df60b650f5f04550560d06ef28cb8f79625
David Laban <david.laban@collabora.co.uk>
2011-06-12 10:18:14 -0400

    Add directory structure.
    * Extract safe_filename() utility for sanitising playlist[folder] names
    * Add string_list (doubly linked list of char *)
    * Add and remove strings to path list, and call mkdir appropriately
      as we traverse the spotify directory structure.
    
    FIXME: Our memory management is shockingly sloppy.

--- src/save.c
+++ src/save.c
@@ -26,6 +26,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
+
+
 
 #include "git-spot.h"
 #include "cmd.h"
@@ -35,6 +38,124 @@ typedef void (*sg_callback) (void *user_data);
 static void container_loaded(sp_playlistcontainer *pc, void *user_data);
 static void save_playlist_async(sp_playlist *playlist, const char *directory,
     sg_callback cb, void *user_data);
+
+static char *safe_filename (const char *str)
+{
+  char *filename = strdup(str);
+  char *c;
+
+  for(c=filename; *c != 0; c++)
+    {
+      if(strchr("/\\. \t+$'\"", *c) != NULL)
+        *c = '_';
+    }
+
+  return filename;
+}
+
+typedef struct _string_list string_list;
+
+struct _string_list {
+  string_list *next;
+  string_list *prev;
+  char *data;
+};
+
+string_list *
+string_list_first(string_list *list)
+{
+  while(list != NULL && list->prev != NULL)
+    list = list->prev;
+  return list;
+}
+
+string_list *
+string_list_last(string_list *list)
+{
+  while(list != NULL && list->next != NULL)
+    list = list->next;
+  return list;
+}
+
+string_list *
+string_list_append(string_list *list, char *data)
+{
+  string_list *new_link = malloc(sizeof(string_list));
+  new_link->next = NULL;
+  new_link->data = data;
+
+  while (list != NULL && list->next != NULL)
+    list = list->next;
+
+  new_link->prev = list;
+
+  if(list != NULL)
+    list->next = new_link;
+
+  return new_link;
+}
+
+string_list *
+string_list_copy(string_list *old_list)
+{
+  string_list *first_link = string_list_append(NULL, old_list->data);
+  string_list *new_link = first_link;
+  string_list *old_link = string_list_first(old_list);
+
+  while (old_link != NULL)
+    {
+      new_link = string_list_append(new_link, strdup(old_link->data));
+      old_link = old_link->next;
+    }
+
+  return first_link;
+}
+
+string_list *
+string_list_remove_tail(string_list *list)
+{
+  string_list *new_tail;
+  if (list == NULL)
+    return NULL;
+  list = string_list_last(list);
+  new_tail = list->prev;
+  new_tail->next = NULL;
+  free(list->data);
+  free(list);
+
+  return new_tail;
+}
+
+static void
+string_list_free(string_list *list)
+{
+  list = string_list_first(list);
+  while(list != NULL)
+    {
+      string_list *next_list = list->next;
+      free(list->data);
+      free(list);
+      list = next_list;
+    }
+}
+
+static char *
+string_list_join(string_list *list, const char *sep)
+{
+  string_list *l;
+  char *old_string = NULL;
+  char *new_string = NULL;
+
+  for (l = string_list_first(list); l != NULL; l = l->next)
+    {
+      asprintf(&new_string, "%s%s%s", old_string, sep, l->data);
+      free(old_string);
+      old_string = new_string;
+    }
+  new_string = strdup(old_string + strlen("(null)") + strlen(sep));
+  free(old_string);
+  return new_string;
+}
 
 typedef struct {
   sp_playlistcontainer *pc;
@@ -111,19 +232,27 @@ static void container_loaded(sp_playlistcontainer *pc, void *userdata)
   int i, j, level = 0;
   sp_playlist *pl;
   char name[200];
+  string_list *path = string_list_append(NULL, strdup(argv[1]));
+
+  if(mkdir(argv[1], 0755) != 0 && errno != EEXIST)
+    printf("WARNING: mkdir('%s') failed.", argv[1]);
 
   container_context_start_call(ctx);
   printf("path = %s\n", argv[1]);
   printf("%d entries in the container\n", sp_playlistcontainer_num_playlists(pc));
 
   for (i = 0; i < sp_playlistcontainer_num_playlists(pc); ++i) {
+    char *folder_name;
     switch (sp_playlistcontainer_playlist_type(pc, i)) {
       case SP_PLAYLIST_TYPE_PLAYLIST:
         printf("%d. ", i);
-        for (j = level; j; --j) printf("\t");
+        folder_name = string_list_join(path, "/");
+        if(mkdir(folder_name, 0755) != 0 && errno != EEXIST)
+          printf("WARNING: mkdir('%s') failed.", folder_name);
         pl = sp_playlistcontainer_playlist(pc, i);
         container_context_start_call(ctx);
-        save_playlist_async(pl, "", (sg_callback)container_context_finish_call, ctx);
+        save_playlist_async(pl, folder_name, (sg_callback)container_context_finish_call, ctx);
+        free(folder_name);
         printf("%s", sp_playlist_name(pl));
         if(subscriptions_updated)
           printf(" (%d subscribers)", sp_playlist_num_subscribers(pl));
@@ -136,8 +265,15 @@ static void container_loaded(sp_playlistcontainer *pc, void *userdata)
         printf("Folder: %s with id %lu\n", name,
              sp_playlistcontainer_playlist_folder_id(pc, i));
         level++;
+        path = string_list_append(path, safe_filename(name));
+
+        folder_name = string_list_join(path, "/");
+        if(mkdir(folder_name, 0755) != 0 && errno != EEXIST)
+          printf("WARNING: mkdir('%s') failed.", folder_name);
+        free(folder_name);
         break;
       case SP_PLAYLIST_TYPE_END_FOLDER:
+        path = string_list_remove_tail(path);
         level--;
          printf("%d. ", i);
         for (j = level; j; --j) printf("\t");
@@ -151,6 +287,7 @@ static void container_loaded(sp_playlistcontainer *pc, void *userdata)
 
   printf("Made %d async calls.\n", ctx->started_calls -1);
   container_context_finish_call(ctx);
+  string_list_free(path);
 }
 
 typedef struct {
@@ -196,18 +333,16 @@ static void save_playlist_finally(playlist_data *data)
 static void actually_save_playlist(playlist_data *data)
 {
   int i;
-  char *filename = strdup(sp_playlist_name(data->playlist));
-  char *c;
-  FILE *output;
+  char *basename = safe_filename(sp_playlist_name(data->playlist));
+   FILE *output;
+  char *filename;
+
+  asprintf(&filename, "%s/%s.json", data->directory, basename);
 
   printf("Playlist '%s' ready.\n", sp_playlist_name(data->playlist));
 
-  for(c=filename; *c != 0; c++)
-    {
-      if(strchr("/\\. \t+$'\"", *c) != NULL)
-        *c = '_';
-    }
   output = fopen(filename, "w+");
+  free(basename);
   free(filename);
 
   fprintf(output, "{'playlist_name': '%s', 'songs': [\n",
